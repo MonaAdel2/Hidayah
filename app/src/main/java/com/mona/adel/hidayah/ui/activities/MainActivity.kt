19f2f873686ad32dd6eefde4ffe20660145165f7
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2024-09-15 02:44:45 +0300

    structure the project and create the packages

--- app/src/main/java/com/mona/adel/hidayah/ui/activities/MainActivity.kt
+++ app/src/main/java/com/mona/adel/hidayah/ui/activities/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.mona.adel.hidayah
+package com.mona.adel.hidayah.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.mona.adel.hidayah.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
